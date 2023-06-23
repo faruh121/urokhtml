2ee0d9fec6ef24fe65eefaec62353e15613dc764
faruh121 <yussupov.fara@gmail.com>
2023-06-23 21:05:47 +0300

    adfs

--- dist/script.js
+++ dist/script.js
@@ -133,3 +133,353 @@
 // } 
 // else {
 //   alert( "логин не зарегестрирован" )
+//dz JS_DZ_Module_1_Week_3
+//   function z1() {
+//     let a = Math.trunc(
+//       +prompt("Задайте целое число, которое будет началом диапазона: ")
+//     );
+//     let a1 = a;
+//     let b;
+//     do {
+//       b = Math.trunc(
+//         +prompt("Задайте целое число, которое будет концом диапазона: ")
+//       );
+//     } while (a >= b);
+//     let summa = 0;
+//     while (a <= b) {
+//       summa += a;
+//       a++;
+//     }
+//     alert(`В диапазоне от ${a1} до ${b} включительно сумма всех чисел составляет: ${summa}.`);
+//   }
+//  z1();
+//   function z2() {
+//     let n = +prompt("Введите первое число: ");
+//     let m = +prompt("Введите второе число: ");
+//     let new_n = Math.abs(n);
+//     let new_m = Math.abs(m);
+//     let max_divisor = 1;
+//     let i = 1;
+//     while (i <= new_n && i <= new_m) {
+//       if (new_n % i == 0 && new_m % i == 0) {
+//         max_divisor = i;
+//       }
+//       i++;
+//     }
+//     alert(`У чисел ${n} и ${m} наибольший общий делитель: ${max_divisor}`);
+//   }
+// z2();
+//   function z3() {
+// let n = +prompt("Введите число: ");
+//     let new_n = Math.abs(n);
+//     let divisors = "";
+//     let i = 1;
+//     do {
+//       if (new_n % i == 0) {
+//         divisors += i + ",";
+//       }
+//       i++;
+//     } while (i <= new_n);
+//     alert(`У числа ${n} следующие делители: ${divisors}`);
+//   }
+// z3();
+//   function z4() {
+//  let n = Math.abs(+prompt("Введите число: "));
+//     let counter = 0;
+//     while (n > 0) {
+//       n = Math.trunc(n / 10);
+//       counter++;
+//     }
+//     alert(`В введенном вами числе (в его целой части) ${counter} цифр.`);
+//   }
+// z4();
+//   function z5() {
+//     let n = 0;
+//     let positive_number = 0;
+//     let negative_number = 0;
+//     let null_number = 0;
+//     let even_number = 0;
+//     let odd_number = 0;
+//     for (let i = 0; i < 10; i++) {
+//       n = +prompt("Введите число: ");
+//       if (n > 0) {
+//         positive_number += 1;
+//       } else if (n < 0) {
+//         negative_number += 1;
+//       } else {
+//         null_number += 1;
+//       }
+//       !(n % 2) ? (even_number += 1) : (odd_number += 1);
+//     }
+//     alert(
+//       `Из 10 введеных вами чисел ${positive_number} положительных, ${negative_number} отрицательных, ${null_number} равных нулю, ${even_number} четных и ${odd_number} нечетных.`
+//     );
+//   }
+// z5();
+//   function z6() {
+//     let numberA = 0;
+//     let numberB = 0;
+//     let result = true;
+//     let action_sign = "";
+//     do {
+//       numberA = +prompt("Введите первое число: ");
+//       numberB = +prompt("Введите второе число: ");
+//       action_sign = prompt("Введите знак операции (*, /, + либо -): ");
+//       switch (action_sign) {
+//         case "+":
+//           alert(
+//             `${numberA} ${action_sign} ${numberB} = ${numberA + numberB}`
+//           );
+//           break;
+//         case "-":
+//           alert(
+//             `${numberA} ${action_sign} ${numberB} = ${numberA - numberB}`
+//           );
+//           break;
+//         case "*":
+//           alert(
+//             `${numberA} ${action_sign} ${numberB} = ${numberA * numberB}`
+//           );
+//           break;
+//         case "/":
+//           alert(
+//             `${numberA} ${action_sign} ${numberB} = ${numberA / numberB}`
+//           );
+//           break;
+//         default:
+//           alert(`Вы не ввели знак математической операции!`);
+//           break;
+//       }
+//       result = confirm("Хотите выполнить еще одну математическую операцию?");
+//     } while (result == true);
+//   }
+// z6();
+//   function z7() {
+// let base_number = prompt("Введите число, которое будем трансформировать: ");
+//     let jump = Math.abs(
+//       Math.trunc(
+//         +prompt("Введите, на сколько позиций вы хотите произвести сдвиг: ")
+//       )
+//     );
+//     let arr = base_number.split("");
+//     jump = jump % arr.length; 
+//     let leftJump_L = arr.slice(0, jump); 
+//     let leftJump_R = arr.slice(jump, arr.length); 
+//     alert(
+//       `Сдвиг влево "${base_number}" на ${jump} позиций:  ` +
+//         leftJump_R.concat(leftJump_L).join("")
+//     ); 
+//     let rightJump_L = arr.slice(0, arr.length - jump); 
+//     let rightJump_R = arr.slice(arr.length - jump, arr.length); 
+//     alert(
+//       `Сдвиг вправо "${base_number}" на ${jump} позиций:  ` +
+//         rightJump_R.concat(rightJump_L).join("")
+//     ); 
+//   }
+//   z7();
+//   function z9() {
+//     let table = "";
+//     for (let i = 2; i <= 9; i++) {
+//       for (let j = 1; j <= 10; j++) {
+//         table += `${i} x ${j} = ` + i * j + ", ";
+//       }
+//     }
+//     alert(table);
+//   }
+//   z9();
+//dz2
+// function a1() {
+//     function Compare(a, b) {
+//       if (a < b) {
+//         return -1;
+//       } else if (a > b) {
+//         return 1;
+//       } else if (a == b) {
+//         return 0;
+//       } else {
+//         return "Вы ошиблись при вводе чисел";
+//       }
+//     }
+//     let x = +prompt("Введите первое число для сравнения ");
+//     let y = +prompt("Введите второе число для сравнения ");
+//     alert(Compare(x, y));
+//   }
+//   a1();
+//   function a2() {
+//     function Factorial(n) {
+//       if (n == 1) return 1;
+//       return n * Factorial(n - 1);
+//     }
+//     let x = 1;
+//     do {
+//       x = Math.trunc(
+//         +prompt("Для вычисления факториала введите положительное число от 1: ")
+//       );
+//     } while (x < 1);
+//     alert(Factorial(x));
+//   }
+//   a2();
+//   function a3() {
+//     function Splitter(a, b, c) {
+//       return a * 100 + b * 10 + c;
+//     }
+//     alert(
+//       "Задание 3: Написать функцию, которая принимает три отдельные цифры и превращает их в одно число. Например: цифры 1, 4, 9 превратятся в число 149."
+//     );
+//     let x = 0;
+//     let y = 0;
+//     let z = 0;
+//     do {
+//       x = Math.trunc(Math.abs(+prompt("Введите первое число: ")));
+//       y = Math.trunc(Math.abs(+prompt("Введите второе число: ")));
+//       z = Math.trunc(Math.abs(+prompt("Введите третье число: ")));
+//     } while (x > 9 || y > 9 || z > 9);
+//     alert(Splitter(x, y, z));
+//   }
+//   a3();
+//   function a4() {
+//     function Square(a, b) {
+//       if (arguments.length == 1) {
+//         return a * a;
+//       }
+//       return a * b;
+//     }
+//     alert(Square(3, 4));
+//     alert(Square(3));
+//   }
+//   a4();
+//   function a5() {
+//     function PerfectNumber(a) {
+//       let summ = 0;
+//       for (let i = 1; i < a; i++) {
+//         if (a % i == 0) {
+//           summ += i;
+//         }
+//       }
+//       if (summ == a) {
+//         return "ДА! Это совершенное число!";
+//       }
+//       return "НЕТ! Это не совершенное число!";
+//     }
+//     let x = Math.trunc(+prompt("Введите число: "));
+//     alert(PerfectNumber(x));
+//   }
+// a5();
+//   function a6() {
+//     function PerfectNumber(a) {
+//       let summ = 0;
+//       a = Math.abs(a);
+//       for (let i = 1; i < a; i++) {
+//         if (a % i == 0) {
+//           summ += i;
+//         }
+//       }
+//       if (summ == a) {
+//         return 1;
+//       }
+//       return 0;
+//     }
+//     let x = 0;
+//     let y = 0;
+//     let result = "";
+//     let perfectNumber = 0;
+//     do {
+//       x = Math.trunc(+prompt("Введите первое число диапазона: "));
+//       y = Math.trunc(+prompt("Введите конечное число диапазона: "));
+//     } while (x != +x || y != +y);
+//     for (let j = x; j <= y; j++) {
+//       perfectNumber = PerfectNumber(j);
+//       if (perfectNumber == 1) {
+//         result += j + ", ";
+//       }
+//     }
+//     if (result == "") {
+//       alert("В данном диапазоне совершенных чисел нет");
+//     } else {
+//       alert(result);
+//     }
+//   }
+// a6();
+//   function a7() {
+//     function Time(a, b = 0, c = 0) {
+//       if (a < 10) {
+//         a = "0" + a;
+//       }
+//       if (b < 10) {
+//         b = "0" + b;
+//       }
+//       if (c < 10) {
+//         c = "0" + c;
+//       }
+//       return `${a}:${b}:${c}`;
+//     }
+//     let x = 0;
+//     let y = 0;
+//     let z = 0;
+//     do {
+//       x = Math.abs(Math.trunc(+prompt("Введите часы от 0 до 23: ")));
+//       y = Math.abs(Math.trunc(+prompt("Введите минуты от 0 до 59: ")));
+//       z = Math.abs(Math.trunc(+prompt("Введите секунды от 0 до 59: ")));
+//     } while (x > 23 || y > 59 || z > 59);
+//     alert("Текущее время: " + Time(x, y, z));
+//   }
+// a7();
+//   function a8() {
+//     function TimeToSeconds(a, b = 0, c = 0) {
+//       let timeToSeconds = a * 3600 + b * 60 + c;
+//       return timeToSeconds;
+//     }
+//     let x = 0;
+//     let y = 0;
+//     let z = 0;
+//     do {
+//       x = Math.abs(Math.trunc(+prompt("Введите часы от 0 до 23: ")));
+//       y = Math.abs(Math.trunc(+prompt("Введите минуты от 0 до 59: ")));
+//       z = Math.abs(Math.trunc(+prompt("Введите секунды от 0 до 59: ")));
+//     } while (x > 23 || y > 59 || z > 59);
+//     alert(
+//       "От начала суток это составляет " + TimeToSeconds(x, y, z) + " секунд."
+//     );
+//   }
+// a8();
+//   function a9() {
+//     function SecondsToTime(a) {
+//       let hours = Math.trunc(a / 3600);
+//       let minutes = Math.trunc((a - hours * 3600) / 60);
+//       let seconds = Math.trunc(a - hours * 3600 - minutes * 60);
+//       if (hours < 10) {
+//         hours = "0" + hours;
+//       }
+//       if (minutes < 10) {
+//         minutes = "0" + minutes;
+//       }
+//       if (seconds < 10) {
+//         seconds = "0" + seconds;
+//       }
+//       let current_time = `${hours}:${minutes}:${seconds}`;
+//       return current_time;
+//     }
+//     let input_seconds = 0;
+//     do {
+//       input_seconds = Math.abs(
+//         Math.trunc(+prompt("Введите секунды для перевода их в текущее время: "))
+//       );
+//     } while (input_seconds != +input_seconds);
+//     alert(SecondsToTime(input_seconds));
+//   }
+//   a9();
+// Создать объект, описывающий автомобиль (производитель,
+//     модель, год выпуска, средняя скорость), и следующие функции
+//     для работы с этим объектом.
+//     1. Функция для вывода на экран информации об автомобиле.
+//     2. Функция для подсчета необходимого времени для преодоления переданного расстояния со средней скоростью.
+//     Учтите, что через каждые 4 часа дороги водителю необходимо делать перерыв на 1 час
+var automobil = [{
+        gorod: 'USA',
+        model: 'dodge',
+        god: '2019',
+        maksspeed: '290'
+    }];
+function showAutomobil(arr) {
+    console.log(arr);
+}
--- script.ts
+++ script.ts
@@ -531,10 +531,161 @@
 //   }
 //   a9();
   
+// Создать объект, описывающий автомобиль (производитель,
+//     модель, год выпуска, средняя скорость), и следующие функции
+//     для работы с этим объектом.
+//     1. Функция для вывода на экран информации об автомобиле.
+//     2. Функция для подсчета необходимого времени для преодоления переданного расстояния со средней скоростью.
+//     Учтите, что через каждые 4 часа дороги водителю необходимо делать перерыв на 1 час
 
-  
-  
+// const automobil = [{
+//     gorod:'USA' ,
+//     model:'dodge',
+//     god:'2019',
+//     speed:'290'
 
+// }]
+
+// function showAutomobil(arr:any[]){
+//     console.log(arr)
+
+
+// function showTravelTime() {
+
+//     var length = prompt("Введите расстояние (км)", "");
+
+//     var time = computeTravelTime(length)
+
+//     console.log("Средняя скорость: " + automobil.speed + " км/ч\n"
+//         + "Расстояние: " + length + " км\n"
+//         + "Время для преодоления: " + time + " ч");
+// }
+
+// function computeTravelTime(length) {
+//     var t = Math.round(
+//         (length / car.averageSpeed) * 100
+//         ) / 100;
+
+//     // находим остаток
+//     var ost = t % 1;
+//     // преобразовуем в минуты
+//     ost = ost * 0.6;
+//     // время (остаток в минутах)
+//     t = Math.floor(t) + ost;
+
+//     // учитываем перерыв на 1 час (каждые 4 часа дороги).
+//     var breakTime = Math.floor(t / 4);
+//     return (t + breakTime).toFixed(2);
+// }
+
+// Создать объект, описывающий время (часы, минуты, секунды), и следующие функции для работы с этим объектом.
+// const time = [{
+//     hh : 20,
+//     mm : 30,
+//     ss : 45
+// }];
+
+
+// function showTime() {
+//     console.log( ( '0' + time.hh).slice(-2) 
+//         + ":"
+//         + ('0' + time.mm).slice(-2)
+//         + ":" 
+//         + ('0' + time.ss).slice(-2)
+//         );
+// }
+
+// function setSecond(second) {
+
+//     // Проверяем нужно ли менять минуты.
+//     if (time.ss + second > 59) {
+//         // узнать сколько минут
+//         let mm = Math.floor(second / 60);
+
+//         // и сколько секунд оставить
+//         if (mm == 0) {
+//             setMinute(1);
+//             second = (time.ss + second) - 60;
+//             time.ss = 0;
+//         }
+//         else{
+//             setMinute(mm);
+//             second = (time.ss + second) - (mm * 60);
+//             time.ss = 0;
+//         }
+//     }
+
+//     // Меняем секунды.
+//     time.ss += second;
+// }
+
+// function setMinute(minute) {
+
+//     // Проверяем нужно ли менять часы.
+//     if (time.mm + minute > 59) {
+//         // узнать сколько часов
+//         var hh = Math.floor(minute / 60);
+
+//         // и сколько минут оставить
+//         if (hh == 0) {
+//             setHour(1);
+//             minute = (time.mm + minute) - 60;
+//             time.mm = 0;
+//         }
+//         else{
+//             setHour(hh);
+//             minute = (time.mm + minute) - (hh * 60);
+//             time.mm = 0;
+//         }
+//     }
+
+//     time.mm += minute;
+// }
+
+// function setHour(hour) {
+//     // Проверяем нужно ли менять счетчик часов.
+//     if (time.hh + hour > 23) {
+//         // узнать сколько дней (дни не выводим и не меняем).
+//         var dd = Math.floor(hour / 24);
+
+//         // и сколько минут оставить
+//         if (dd == 0) {
+//             hour = (time.hh + hour) - 24;
+//             time.hh = 0;
+//         }
+//         else{
+//             hour = (time.hh + hour) - (dd * 24);
+//             time.hh = 0;
+//         }
+//     }
+
+//     time.hh += hour;
+// }
+
+
+
+// function changeSeconds() {
+//     var ss = prompt("Введите кол-во секунд для изменения времени", "");
+
+//     setSecond(+ss);
+
+//     showTime();
+// }
+
+// function changeMinutes() {
+//     var mm = prompt("Введите кол-во минут для изменения времени", "");
+
+//     setMinute(+mm);
+
+//     showTime();
+// }
+
+// function changeHours() {
+//     var hh = prompt("Введите кол-во часов для изменения времени", "");
+
+//     setHour(+hh);
+
+//     showTime();
   
 
 
